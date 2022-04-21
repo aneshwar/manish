714f6f21e14b94a9318e19f1539d8efc61a96827
aneshwar <aneshwaryadavyadav@gmail.com>
2022-04-21 00:48:14 -0700

    Insial commit

---
+++ Documents/vercel/api/index.js
@@ -0,0 +1,12 @@
+// require('dotenv').config()
+const express = require('express')
+const product = require('./api/product')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use("/api/product", product);
+
+app.listen(PORT, () => {
+    console.log(`concted: ${PORT}`)
+})
